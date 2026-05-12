14f5a25aa333eb1b994fdbb7b4a971f57092a29c
yAly999 <alysson.delpawlack@gmail.com>
2026-05-12 14:21:21 -0300

    .

--- relogio.py
+++ relogio.py
@@ -10,5 +10,28 @@ def shutdown():
             os.system("shutdown /s /t 0")
         elif "linux" in sistema or "darwin" in sistema:
             os.system("shutdown -h now")    
+        else :
+            print("sistema operacional não suportado para shutdown automático.")    
     except Exception as e:
-        print(f"Error occurred while trying to shutdown: {e}")
+        print(f"Erro ao tentar desligar o computador: {e}")
+
+def temporizador():
+    print("=== temporizador ===")
+    try:
+        entrada = int(input("Digite o tempo em segundos para o temporizador: "))
+        segundos = int(entrada)
+
+        while segundos > 0:
+            mins, secs = divmod(segundos, 60)
+            timer = f'{mins:02d}:{secs:02d}'
+
+        bip = "\a" if 0 < segundos <= 10 else "" 
+        print(f"Tempo restante: {timer} {bip}", end="", flush=True)
+        time.sleep(1)
+        segundos -= 1
+
+        print("\n\n iniciando o desligamento automático...")
+
+        shutdown()
+    except ValueError:
+        print("Entrada inválida. Por favor, digite um número inteiro para os segundos.")
