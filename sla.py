770b2fa65636cc7c7ef9d2fb2895ceee31f82e5b
yAly999 <alysson.delpawlack@gmail.com>
2026-05-12 17:12:38 -0300

    ççç

--- sla.py
+++ sla.py
@@ -3,10 +3,16 @@ import tkinter as tk
 root = tk.Tk()
 root.title("simulador de investimentos - Sicredi")
 root.geometry("400x420")
-root.configure(bg="#005c36")
+root.configure(bg="#004363")
 root.resizable(False, False)
 
-tk.Label(root, text="Simulador de Investimentos", font=("Arial", 14), bg="#005c36", fg="white").pack(pady=20)
+tk.Label(root, text="Simulador de Investimentos", font=("Arial", 14), bg="#004363", fg="White").pack(pady=20)
+
+tkLabel = tk.Label(root, text="sicredi", font=("Arial", 14, "italic"), bg="#004363", fg="White").pack(pady=10)
+
+tk.label = tk.Label(root,text="Valor inicial($):", font=("Arial",12), bg="#004363", fg="White").pack(anchor= "center")
+
+entrada_principal = tk.Entry(root, font=("Arial", 12), width=20,relief="groove").pack(pady5)
 
 
 
